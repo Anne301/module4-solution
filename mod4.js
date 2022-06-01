e463138bbd0dd024028bc14e75b416562d57123c
Anne Hamelink <annehamelink@mbp-van-anne.home>
2022-06-01 19:00:54 +0200

    final

--- mod4.js
+++ mod4.js
@@ -18,23 +18,15 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // Loop though the names
 for (var n = 0; n < names.length; n++) {
 
-
 	// Find first letter of name	
   	var firstLetter = names[n].charAt(0).toLowerCase();
 
-  	// set default speakword to "hello".
-  	var speakWord = "Hello";
-
   	// if the name starts with a J, say goodbye
 	if (firstLetter === 'j') {
-
-		speakWord = 'Good bye';
-		console.log(speakWord+" "+names[n]);
-
+		byeSpeaker.speak(names[n]);
 	} 
 	else { // say hello, default
-
-		console.log(speakWord+" "+names[n]);
+		helloSpeaker.speak(names[n]);
 
 	}
 } // end forloop
